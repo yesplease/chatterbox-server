97a474d48cc0688c045423346518d8fb82d3aa77
yesplease <bradley.heinz@gmail.com>
2015-02-24 19:55:07 -0800

    Add ability to save to permanent file

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -29,14 +29,14 @@ $(function() {
       app.$roomSelect.on('change', app.saveRoom);
 
       // Fetch previous messages
-      app.startSpinner();
+      //app.startSpinner();
       app.fetch(false);
 
       // Poll for new messages
-      setInterval(app.fetch, 3000);
+      setInterval(app.fetch, 5000);
     },
     send: function(data) {
-      app.startSpinner();
+      //app.startSpinner();
       // Clear messages input
       app.$message.val('');
 
@@ -47,7 +47,6 @@ $(function() {
         data: JSON.stringify(data),
         contentType: 'application/json',
         success: function (data) {
-          console.dir(data);
           console.log('chatterbox: Message sent');
           // Trigger a fetch to update the messages, pass true to animate
           app.fetch();
@@ -74,7 +73,7 @@ $(function() {
           // Get the last message
           var mostRecentMessage = data.results[data.results.length-1];
           var displayedRoom = $('.chat span').first().data('roomname');
-          app.stopSpinner();
+          // app.stopSpinner();
           app.populateMessages(data.results, animate);
           app.populateRooms(data.results);
           //Fred's extra credit testing of unique messages:
@@ -103,7 +102,7 @@ $(function() {
       // Clear existing messages
 
       app.clearMessages();
-      app.stopSpinner();
+      //app.stopSpinner();
       if (Array.isArray(results)) {
         // Add all fetched messages
         results.forEach(app.addMessage);
@@ -208,7 +207,7 @@ $(function() {
         }
       }
       else {
-        app.startSpinner();
+        //app.startSpinner();
         // Store as undefined for empty names
         app.roomname = app.$roomSelect.val();
 
@@ -228,14 +227,14 @@ $(function() {
       // Stop the form from submitting
       evt.preventDefault();
     },
-    startSpinner: function(){
-      $('.spinner img').show();
-      $('form input[type=submit]').attr('disabled', "true");
-    },
+    // startSpinner: function(){
+    //   $('.spinner img').show();
+    //   $('form input[type=submit]').attr('disabled', "true");
+    // },
 
-    stopSpinner: function(){
-      $('.spinner img').fadeOut('fast');
-      $('form input[type=submit]').attr('disabled', null);
-    }
+    // stopSpinner: function(){
+    //   $('.spinner img').fadeOut('fast');
+    //   $('form input[type=submit]').attr('disabled', null);
+    // }
   };
 }());
--- server/request-handler.js
+++ server/request-handler.js
@@ -1,3 +1,5 @@
+var fs = require("fs");
+
 var headers = {
   "access-control-allow-origin": "*",
   "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
@@ -5,97 +7,96 @@ var headers = {
   "access-control-max-age": 10, // Seconds.
   "Content-Type": "application/JSON"
 };
-/*************************************************************
 
-You should implement your request handler function in this file.
+var objectId = 1;
+var results = [
+  // Useful for debugging
+  // {
+  //   text: 'hello world',
+  //   username: 'fred',
+  //   objectId: objectId
+  // }
+];
 
-requestHandler is already getting passed to http.createServer()
-in basic-server.js, but it won't work as is.
-
-You'll have to figure out a way to export this function from
-this file and include it in basic-server.js so that it actually works.
-
-*Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
-
-**************************************************************/
-
-var results = [];
-// var messages = {
-//   roomname: results = [],
-//   lobby:
-//   whatever:
-// },
 exports.requestHandler = function(request, response) {
-  // Request and Response come from node's http module.
-  //
-  // They include information about both the incoming request, such as
-  // headers and URL, and about the outgoing response, such as its status
-  // and content.
-  //
-  // Documentation for both request and response can be found in the HTTP section at
-  // http://nodejs.org/documentation/api/
 
-  // Do some basic logging.
-  //
-  // Adding more logging to your server can be an easy way to get passive
-  // debugging help, but you should always be careful about leaving stray
-  // console.logs in your code.
   console.log("Serving request type " + request.method + " for url " + request.url);
 
   // The outgoing status.
   var statusCode = 200;
 
-  // See the note below about CORS headers.
-
-  // Tell the client we are sending them plain text.
-  //
-  // You will need to change this if you are sending something
-  // other than plain text, like JSON or HTML.
-
-  // .writeHead() writes to the request line and headers of the response,
-  // which includes the status and all headers.
-  response.writeHead(statusCode, headers);
 
   var parseURL = function(url){
     var substring = url.substring(0,9)
-    console.log("This is the substring", substring);
     if (substring !== '/classes/'){
       response.writeHead(404, headers);
       response.end();
     }
   };
-  //parseURL(request.url);
+
+  var sendResponse = function(data, statusCode){
+    statusCode = statusCode || 200;
+    response.writeHead(statusCode, headers);
+    response.end(JSON.stringify(data));
+  };
+
+  var collectData = function(request, callback){
+    var data = "";
+    request.on('data', function(chunk){
+      data += chunk;
+    });
+    request.on('end', function(){
+      callback(JSON.parse(data));
+    });
+  };
+
+  // var actions = {
+  //   'GET': function(request, response){
+
+  //   },
+  //   'POST': somethingElse,
+  //   'OPTIONS': something
+  // };
 
   if (request.method === 'GET') {
-    console.log("This is the request url: ", request.url);
-    //var dummyResponse = {results: ["yes", "no"]};
-    // if (!allOurData[]) {
-    //   response.writeHead(404, headers);
-    // }
+    //console.log("This is the request url: ", request.url);
     parseURL(request.url);
-    var responseObj = {};
-    responseObj['results'] = results;
-    var stringResults = JSON.stringify(responseObj);
-    //response.write(stringDummy);
-    console.log(results);
-    response.end(stringResults); //we can send a json stringified object back
-    // console.log("The request method is 'get'!");
+
+    sendResponse({results: results}, 200);
   }
 
   if (request.method === 'POST') {
-    var newData = '';
-    request.on('data', function(data){
-      newData += data;
+    var msg;
+    collectData(request, function(message){
+      message.objectId = ++objectId;
+      msg = message;
+      results.push(message);
+      saving(msg);
+      sendResponse("dummydata", 201);
     });
-    request.on('end', function(){
-     var parsed = JSON.parse(newData);
-     results.push(parsed);
-     console.log("We just posted this!", parsed);
-     response.writeHead(201, headers);
-     response.end();
+
+   var saving = function(msg) {
+     var logResult = JSON.stringify(msg);
+     fs.appendFile("log.txt", logResult, function (err) {
+      if (err) return console.log(err);
+      console.log('Successfully saved to log.txt');
     });
-    //console.log(request);
-   console.log("This is the status code " + statusCode);
+   }
+
+   // setTimeout(saving(), 3000);
+   console.log("This is results right before saving", results);
+   //saving();
+
+    // var newData = '';
+    // request.on('data', function(data){
+    //   newData += data;
+    // });
+    // request.on('end', function(){
+    //  var parsed = JSON.parse(newData);
+    //  results.push(parsed);
+    //  response.writeHead(201, headers);
+    //  response.end();
+    // });
   }
 
   if (request.method === 'OPTIONS'){
@@ -112,13 +113,4 @@ exports.requestHandler = function(request, response) {
   // node to actually send all the data over to the client.
 };
 
-// These headers will allow Cross-Origin Resource Sharing (CORS).
-// This code allows this server to talk to websites that
-// are on different domains, for instance, your chat client.
-//
-// Your chat client is running from a url like file://your/chat/client/index.html,
-// which is considered a different domain.
-//
-// Another way to get around this restriction is to serve you chat
-// client from this domain by setting up static file serving.
 
